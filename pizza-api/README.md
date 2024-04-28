ae4d1d34f2cc1048516004afcf834d551487c27f
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-04-28 16:35:07 +0000

    S01E01

--- pizza-api/README.md
+++ pizza-api/README.md
@@ -1,5 +1,5 @@
-# pizzas-api
-# 
+# pizza-api
+
 This project uses Quarkus, the Supersonic Subatomic Java Framework.
 
 If you want to learn more about Quarkus, please visit its website: https://quarkus.io/ .
@@ -43,7 +43,7 @@ Or, if you don't have GraalVM installed, you can run the native executable build
 ./mvnw package -Dnative -Dquarkus.native.container-build=true
 ```
 
-You can then execute your native executable with: `./target/pizzas-api-1.0.0-SNAPSHOT-runner`
+You can then execute your native executable with: `./target/pizza-api-1.0.0-SNAPSHOT-runner`
 
 If you want to learn more about building native executables, please consult https://quarkus.io/guides/maven-tooling.
 
---
+++ pizza-api/src/main/java/academy/quarkus/pizza/PizzaResource.java
@@ -0,0 +1,30 @@
+package academy.quarkus.pizza;
+
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.event.Observes;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/pizza")
+public class PizzaResource {
+
+    @Transactional
+    public void init(@Observes StartupEvent ev){
+        var p1 = new Pizza("Marguerita");
+        p1.persist();
+
+        var p2 = new Pizza("Mushrooms");
+        p2.persist();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Pizza> getPizza() {
+        return Pizza.listAll();
+    }
+}
