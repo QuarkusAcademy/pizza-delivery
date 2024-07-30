8dc5f52f2e049edb1b29b9aaff2f6a030ece6211
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-07-30 09:11:48 +0000

    wip

--- pizza-api/src/main/java/academy/quarkus/pizza/rs/RootResource.java
+++ pizza-api/src/main/java/academy/quarkus/pizza/rs/RootResource.java
@@ -5,12 +5,12 @@ import java.util.Map;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
+import static jakarta.ws.rs.core.MediaType.*;
 
 @Path("")
 public class RootResource {
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
     public Map<String, String> get(){
         return Map.of("message", "Welcome to our Pizza Delivery API!");
     }
